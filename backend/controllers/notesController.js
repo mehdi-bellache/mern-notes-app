fcaf8e72a405d731e88ca26074001c1794022b68
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-21 19:55:56 +0200

    Extract notes logic to notesController

---
+++ backend/controllers/notesController.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+export const getAllNotes = (req, res) => {
+  res.send("All notes");
+};
+
+export const getNote = (req, res) => {
+  res.send("get one note");
+};
+
+export const createNote = (req, res) => {
+  res.json({ message: "note created" });
+};
+
+export const updateNote = (req, res) => {
+  res.json({ message: "note updated successfully" });
+};
+
+export const deleteNote = (req, res) => {
+  res.json({ message: "note deleted successfully" });
+};
