a4a2added0db5f2b4712a6e9b3131cb612274ac9
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-21 18:41:07 +0200

    Add different routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
+// get All notes
 app.get("/api/v1/notes", (req, res) => {
   res.send("All notes");
+});
+
+// get one note
+app.get("/api/v1/notes/:id", (req, res) => {
+  res.send("get one note");
+});
+
+// create a note
+app.post("/api/v1/notes", (req, res) => {
+  res.json({ message: "note created" });
+});
+
+// update a note
+app.patch("api/v1/notes/:id", (req, res) => {
+  res.json({ message: "note updated successfully" });
+});
+
+// delete a note
+app.delete("api/v1/notes/:id", (req, res) => {
+  res.json({ message: "note deleted successfully" });
 });
 
 app.listen(3000, () => {
