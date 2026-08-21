8044ee39c945e2b480b905182771c8fa4da45336
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-21 19:18:31 +0200

    Add the folder routes with the file notesRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,34 @@
 const express = require("express");
+const notesRoutes = require("./routes/notesRoutes");
 
 const app = express();
 
-// get All notes
-app.get("/api/v1/notes", (req, res) => {
-  res.send("All notes");
-});
+app.use("/api/v1/notes", notesRoutes);
 
-// get one note
-app.get("/api/v1/notes/:id", (req, res) => {
-  res.send("get one note");
-});
+// // get All notes
+// app.get("/api/v1/notes", (req, res) => {
+//   res.send("All notes");
+// });
 
-// create a note
-app.post("/api/v1/notes", (req, res) => {
-  res.json({ message: "note created" });
-});
+// // get one note
+// app.get("/api/v1/notes/:id", (req, res) => {
+//   res.send("get one note");
+// });
 
-// update a note
-app.patch("api/v1/notes/:id", (req, res) => {
-  res.json({ message: "note updated successfully" });
-});
+// // create a note
+// app.post("/api/v1/notes", (req, res) => {
+//   res.json({ message: "note created" });
+// });
 
-// delete a note
-app.delete("api/v1/notes/:id", (req, res) => {
-  res.json({ message: "note deleted successfully" });
-});
+// // update a note
+// app.patch("api/v1/notes/:id", (req, res) => {
+//   res.json({ message: "note updated successfully" });
+// });
+
+// // delete a note
+// app.delete("api/v1/notes/:id", (req, res) => {
+//   res.json({ message: "note deleted successfully" });
+// });
 
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("all notes");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("get one note");
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "note created" });
+});
+
+router.patch("/:id", (req, res) => {
+  res.json({ message: "note updated successfully" });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ message: "note deleted successfully" });
+});
+
+module.exports = router;
