c3c57443c12974f47f04da305fd9857ae15c6c0a
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-22 18:33:09 +0200

    Import pages and set up routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Route, Routes} from "react-router";
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/create" element={<CreatePage/>}/>
+        <Route path="/note/:id" element={<NoteDetailPage/>}/>
+      </Routes>
       
     </div>
   )
