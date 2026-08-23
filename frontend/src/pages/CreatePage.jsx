e566cac256a5902186b5b62c2ddc9c70fbf25814
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-23 15:14:12 +0200

    Add the button Back to Notes in createPage file

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { ArrowLeftIcon } from 'lucide-react';
+import React, { useState } from 'react'
 
 function CreatePage() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {};
   return (
-    <div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+          <Link to={"/"} className="btn btn-ghost mb-6">
+          <ArrowLeftIcon className='size-5'/>
+          Back to Notes
+
+          </Link>
+        </div>
+      </div>
+
       
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dark", "cyberpunk", "aqua", "coffee"],
+    themes: ["dark", "aqua", "cyberpunk", "coffee"],
   },
 };
