7a984f5571068cff03d63f699169ef3001a8f168
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-23 19:58:17 +0200

    Add a useEffect in NoteDetailPage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 import TopNavbar from '../components/TopNavbar';
 import RateLimitedUI from '../components/RateLimitedUI';
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 
 function HomePage() {
@@ -48,6 +49,8 @@ function HomePage() {
         { isRateLimited && <RateLimitedUI/>}
         <div className='max-w-7xl mx-auto p-4 mt-6'>
           {loading && <div className="text-center text-primary py-10"> Loading notes...</div>}
+
+          {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
 
           {notes.length > 0 && !isRateLimited && (
             <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router';
+import { LoaderIcon } from 'lucide-react';
 
 function NoteDetailPage() {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+  const {id }= useParams();
+
+  useEffect(()=>{
+    const fetchNote = async() =>{
+      try{
+        const res = await api.get(`/notes/${id}`)
+        setNote(res.data);
+
+      }
+      catch(error){
+        toast.error("Failed to fetch the note");
+      }
+      finally{
+        setLoading(false);
+
+      }
+    }; fetchNote();
+  }, [id])
+
+  if(loading){
+    return (
+      <div className='min-h-screen bg-base-200 flex items-center justify-center'>
+        <LoaderIcon className="animate-spin size-10"/>
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container'></div>
       
     </div>
   )
