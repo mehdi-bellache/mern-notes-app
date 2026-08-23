6066b9565fdd7ca0f9277f200515bf1464cb8efc
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-23 15:23:30 +0200

    Add name input field to CreatePage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -11,10 +11,24 @@ function CreatePage() {
       <div className='container mx-auto px-4 py-8'>
         <div className='max-w-2xl mx-auto'>
           <Link to={"/"} className="btn btn-ghost mb-6">
-          <ArrowLeftIcon className='size-5'/>
-          Back to Notes
-
+            <ArrowLeftIcon className='size-5'/>
+            Back to Notes
           </Link>
+          <div className='card bg-base-200'>
+            <div className='card-body'>
+              <h2 className='card-title text-2xl mb-4'>Create New Note</h2>
+              <form onSubmit={handleSubmit}></form>
+              <div className='form-control mb-4'>
+                <label className='label'>
+                  <span className='label-text'>Title</span>
+                </label>
+                <input type="text" placeholder='Note Title' className='input input-bordered' value={title} onChange={(e) => setName(e.target.value)} />
+              </div>
+              <div className='form-control mb-4'>
+                <label className='labes'></label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
