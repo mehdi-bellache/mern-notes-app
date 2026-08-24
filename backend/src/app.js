382f4cdb7958e108b22b389ef3763391fb4b3661
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-24 12:30:54 +0200

    import path and use it in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./db/connect.js";
@@ -8,15 +9,26 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/v1/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3000;
 
